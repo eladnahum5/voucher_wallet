7c2a9302930d8390e026a79dfb3960ae5858bf8e
Elad Nahum <eladnahum5@gmail.com>
2026-03-06 00:42:45 +0200

    init value

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ from .const import DOMAIN
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Setup our skeleton component."""
     # States are in the format DOMAIN.OBJECT_ID.
-    hass.states.async_set('voucher_wallet.Hello_World', 'Works!')
+    hass.states.async_set('voucher_wallet.total_value', 0)
 
     # Return boolean to indicate that initialization was successfully.
     return True
