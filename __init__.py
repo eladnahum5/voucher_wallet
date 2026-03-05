fe0e7b3028fa25693638e7722ff276bf8421cd73
Elad Nahum <eladnahum5@gmail.com>
2026-03-06 00:14:34 +0200

    remove import asyncio

--- __init__.py
+++ __init__.py
@@ -1,25 +1,11 @@
-"""
-The "hello world" custom component.
-
-This component implements the bare minimum that a component should implement.
-
-Configuration:
-
-To use the hello_world component you will need to add the following to your
-configuration.yaml file.
-
-voucher_wallet:
-"""
 from __future__ import annotations
 
-import asyncio
 
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
 
 from .const import DOMAIN
 
-@asyncio.coroutine
 def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Setup our skeleton component."""
     # States are in the format DOMAIN.OBJECT_ID.
