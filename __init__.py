c30d042eff7defd236d807895971e3ed5be089cb
Elad Nahum <eladnahum5@gmail.com>
2026-03-06 01:40:04 +0200

    added view. last before night

--- __init__.py
+++ __init__.py
@@ -6,6 +6,7 @@ from homeassistant.helpers.typing import ConfigType
 
 from .const import DOMAIN
 from .sqlite_helper import VoucherWalletDatabase
+from .views import AddVoucherView, RemoveVoucherView
 
 import sqlite3
 
@@ -17,6 +18,9 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
 
     db = VoucherWalletDatabase(hass)  # Initialize the database
     hass.data[DOMAIN] = {"db": db}  # Store the database instance in hass.data for later use
+
+    hass.http.register_view(AddVoucherView)
+    hass.http.register_view(RemoveVoucherView)
 
     # Return boolean to indicate that initialization was successfully.
     return True
