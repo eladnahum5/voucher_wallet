9c7f549fc1e1b3f3ae789db3a96b1831aa71dedd
Elad Nahum <eladnahum5@gmail.com>
2026-03-05 23:32:13 +0200

    to voucher wallet

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ Configuration:
 To use the hello_world component you will need to add the following to your
 configuration.yaml file.
 
-hello_world_async:
+voucher_wallet:
 """
 from __future__ import annotations
 
@@ -18,14 +18,14 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
 
 # The domain of your component. Should be equal to the name of your component.
-DOMAIN = "hello_world_async"
+DOMAIN = "voucher_wallet"
 
 
 @asyncio.coroutine
 def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Setup our skeleton component."""
     # States are in the format DOMAIN.OBJECT_ID.
-    hass.states.async_set('hello_world_async.Hello_World', 'Works!')
+    hass.states.async_set('voucher_wallet.Hello_World', 'Works!')
 
     # Return boolean to indicate that initialization was successfully.
     return True
