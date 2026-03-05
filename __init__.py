3a5f743ec9bf695f22734edfbe86a199ac69992f
Elad Nahum <eladnahum5@gmail.com>
2026-03-06 01:41:55 +0200

    .

--- __init__.py
+++ __init__.py
@@ -12,11 +12,7 @@ import sqlite3
 
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
-    """Setup our skeleton component."""
-    old_states = [s.entity_id for s in hass.states.async_all() 
-                  if s.entity_id.startswith(f"{DOMAIN}.")]
-    for state in old_states:
-        hass.states.async_remove(state)
+    """Set up the Voucher Wallet integration."""
 
     db = VoucherWalletDatabase(hass)  # Initialize the database
     hass.data[DOMAIN] = {"db": db}  # Store the database instance in hass.data for later use
